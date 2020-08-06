b22e9fc90ce586fe81aa1ad3d5654ef134cfe9e7
foxten <tenaysiafox@gmail.com>
2020-08-05 19:31:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email){
+    let responseObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: name, email: email})
+    }
+    return fetch('http://localhost:3000/users', responseObj)
+        .then(response => response.json())
+        .then(newEntry => {
+            let pTag = document.createElement('p')
+            pTag.innerHTML = newEntry.id
+            document.body.appendChild(pTag)
+        })
+        .catch(error =>{
+            document.body.innerHTML = error.message
+        })
+}
